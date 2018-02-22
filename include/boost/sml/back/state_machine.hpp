e08d2cb0e5d299223c21c91368af6cf185e07c63
Kris Jusiak <kris@jusiak.net>
2018-02-22 18:47:17 +0000

    fixup! only msvc 2017

--- include/boost/sml/back/state_machine.hpp
+++ include/boost/sml/back/state_machine.hpp
@@ -347,7 +347,9 @@ class sm {
       aux::conditional_t<aux::is_same<no_policy, logger_t>::value, aux::type_list<>, aux::type_list<logger_t &>>;
   using transitions_t = decltype(aux::declval<sm_t>().operator()());
 
+#if !defined(_MSC_VER) // __pph__
   static_assert(concepts::composable<sm_t>::value, "Composable constraint is not satisfied!");
+#endif // __pph__
 
  public:
   using states = aux::apply_t<aux::unique_t, aux::apply_t<get_states, transitions_t>>;
